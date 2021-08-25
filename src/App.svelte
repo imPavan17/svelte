5f9c5ffaaf49a99e2eb0c3b5f8fbb1848579ce19
Pavan <pavans@geekyants.com>
2021-08-25 22:23:30 +0530

    base version

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,9 @@
 <script>
-	let firstName = "John";
-	let lastName = 'Cena';
-	let beltColor = 'black';
-
-	/** Reactive values - Which are reactive, they update automatically, when the data they depend on changes. It watches certain values and update in the FE.*/
-	$: fullName = `${firstName} - ${lastName}`; 
-	$: console.log(beltColor) // Reactive statements
-	/** We can also include multiple reactive statements*/
-	$: {
-		console.log(beltColor);
-		console.log(fullName);
-	}
+	
 </script>
 
 <main>
-	<p>{fullName} - {beltColor} belt</p>
-	<input type="text" bind:value={firstName}/>
-	<input type="text" bind:value={lastName}/>
-	<input type="text" bind:value={beltColor}/>
+	
 </main>
 
 <style>
@@ -26,13 +12,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
