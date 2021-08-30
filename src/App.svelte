6f0a3b58d5613ad70a52bbbb35aa76518b6110d4
Pavan <pavans@geekyants.com>
2021-08-30 23:13:39 +0530

    props

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
-	import ReactiveValues from './concepts/ReactiveValues.svelte';
-	import Loops from './concepts/Loops.svelte';
-	import InlineEventHandlers from './concepts/InlineEventHandlers.svelte';
-	import Conditionals from './concepts/Conditionals.svelte';
+	// import ReactiveValues from './concepts/ReactiveValues.svelte';
+	// import Loops from './concepts/Loops.svelte';
+	// import InlineEventHandlers from './concepts/InlineEventHandlers.svelte';
+	// import Conditionals from './concepts/Conditionals.svelte';
+
+	import Modal from './Modal.svelte';
 </script>
 
 <!-- <ReactiveValues /> -->
 <!-- <Loops /> -->
 <!-- <InlineEventHandlers /> -->
-<Conditionals />
+<!-- <Conditionals /> -->
+
+<Modal message={'Hey, i am a prop value'} isPromo={true}/>
---
+++ src/Modal.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let message;
+    export let isPromo = false;
+    let showModal = true;
+
+    console.log(message);
+</script>
+
+{#if showModal}
+<!-- class: promo={boolValue} -> States that boolValue is truthy, then add the className called promo to div-->
+    <div class="backdrop" class:promo={isPromo}> 
+        <div class="modal">
+            <p>{message}</p>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.8);
+    }
+
+    .modal {
+        padding: 10px;
+        margin: 10% auto;
+        border-radius: 10px;
+        max-width: 400px;
+        text-align: center;
+        background-color: white;
+    }
+
+    .promo .modal {
+        background-color: crimson;
+        color: white;
+    }
+</style>
