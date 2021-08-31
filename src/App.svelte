5d417d5f8d3cf6a13d543dd29d0c004b3e1a6b87
Pavan <pavans@geekyants.com>
2021-08-31 12:14:35 +0530

    event forwarding | event modifiers | slots

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,12 @@
 	// import Conditionals from './concepts/Conditionals.svelte';
 
 	import Modal from './Modal.svelte';
+
+	let showModal = false;
+
+	const toggleModal = () => {
+		showModal = !showModal;
+	}
 </script>
 
 <!-- <ReactiveValues /> -->
@@ -12,4 +18,11 @@
 <!-- <InlineEventHandlers /> -->
 <!-- <Conditionals /> -->
 
-<Modal message={'Hey, i am a prop value'} isPromo={true}/>
+<Modal {showModal} on:click={toggleModal}>
+	<h2>Add new person.</h2>
+	<form>
+		<input type="text" placeholder="Name"/>
+		<input type="text" placeholder="Belt Color"/>
+	</form>
+</Modal>
+<button on:click={toggleModal}>Open</button>
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,16 +1,20 @@
 <script>
     export let message;
+	export let showModal = false;
     export let isPromo = false;
-    let showModal = true;
 
-    console.log(message);
+    /** Event Modifiers*/
+    // once - makes sure the event can only fire once(removes handler)
+    // preventDefault - prevent the default action(run e.preventDefault)
+    // self - only fires the event if the clicked element is the target.
+
 </script>
 
 {#if showModal}
 <!-- class: promo={boolValue} -> States that boolValue is truthy, then add the className called promo to div-->
-    <div class="backdrop" class:promo={isPromo}> 
+    <div class="backdrop" class:promo={isPromo} on:click|self> 
         <div class="modal">
-            <p>{message}</p>
+            <slot></slot>
         </div>
     </div>
 {/if}
